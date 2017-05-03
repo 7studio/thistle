d6f761e16d9a7c7976eb4f0521700f8819c7cef2
Xavier Zalawa <xavier@7studio.fr>
2017-05-03 16:27:51 +0200

    Add support for Giphy oEmbed

--- includes/embed.php
+++ includes/embed.php
@@ -540,5 +540,72 @@ if ( ! function_exists( 'thistle_add_oembed_data_post_meta' ) ) {
         return $data;
     }
 }
-
 add_filter( 'oembed_result', 'thistle_add_oembed_data_post_meta', 10, 3 );
+
+if ( ! function_exists( 'thistle_oembed_add_giphy_provider' ) ) {
+    /**
+     * Adds support for Giphy oEmbed.
+     *
+     * @link https://github.com/iamcal/oembed/issues/224
+     * @link https://github.com/Giphy/GiphyAPI/issues/76#issuecomment-220087715
+     * @link https://giphy.com/posts/how-to-embed-giphy-gifs-on-your-website
+     */
+    function thistle_oembed_add_giphy_provider() {
+        $endpoints = array(
+            '#https?://media\.giphy\.com/.*/giphy\.gif#i' => 'https://giphy.com/services/oembed',
+            '#https?://(www\.)?giphy\.com/gifs/.*#i'      => 'https://giphy.com/services/oembed',
+            '#https?://gph\.is/.*#i'                      => 'https://giphy.com/services/oembed'
+        );
+
+        foreach ( $endpoints as $pattern => $endpoint ) {
+            wp_oembed_add_provider( $pattern, $endpoint, true );
+        }
+    }
+}
+add_action( 'init', 'thistle_oembed_add_giphy_provider' );
+
+if ( ! function_exists( 'thistle_giphy_oembed_dataparse' ) ) {
+    /**
+     * Overrides the returned oEmbed HTML for the GIPHY provider.
+     *
+     * By default, Giphy chooses to handle their GIFs as "photo" instead of
+     * "rich" content through their oEmbed API. It's acceptable but it's also
+     * a shame when you know that Giphy suggests to share their content via
+     * an `<iframe>` on their website.
+     *
+     * This hook operates before the default Thistle one.
+     *
+     * @param string $return The returned oEmbed HTML.
+     * @param object $data   A data object result from an oEmbed provider.
+     * @param string $url    The URL of the content to be embedded.
+     * @return string HTML needed to embed.
+     */
+    function thistle_giphy_oembed_dataparse( $return, $data, $url ) {
+        if ( $return === false ) {
+            return $return;
+        }
+
+        if ( $data->provider_name == 'GIPHY'
+            && preg_match( '/https?:\/\/media\.giphy\.com\/media\/(?<key>[^ \/]+)\/giphy\.gif/i', $data->url, $matches ) )
+        {
+            $key = $matches['key'];
+
+            $return = <<<GIPHY
+<iframe src="//giphy.com/embed/{$key}?html5=true"
+        width="{$data->width}"
+        height="{$data->height}"
+        frameBorder="0"
+        class="giphy-embed"
+        allowFullScreen
+>
+</iframe>
+<p>
+    <a href="{$data->url}">via GIPHY</a>
+</p>
+GIPHY;
+        }
+
+        return $return;
+    }
+}
+add_filter( 'oembed_dataparse', 'thistle_giphy_oembed_dataparse', 9, 3 );
